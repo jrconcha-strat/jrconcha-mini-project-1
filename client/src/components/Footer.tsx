57cded02b91b8580b77ad8ed6e8fc9ffdcdac596
Miyukiin <jr.concha99@gmail.com>
2025-07-01 14:46:04 +0800

    Feature: Populate Footer component

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,3 +1,9 @@
 export default function Footer() {
-  return <></>;
+  return (
+    <footer className="h-[80px] bg-[var(--background)] filter brightness-115 p-4 shadow-[0px_-5px_10px_rgba(0, 0, 0, 0.2)]">
+      <div className="navbar flex flex-[1_1_auto] justify-center h-full mx-10 items-center">
+        <p className="text-center sm:text-sm md:text-base lg:text-base ">© 2025 Jhack Concha. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 }
