96dc7417d8636d7955e9adbcd148aee27e8391d8
Miyukiin <jr.concha99@gmail.com>
2025-07-03 19:58:08 +0800

    Refactor: Change method of display for dropdown menu

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,23 +5,8 @@ import NavigationMenuCaller from "./ui/NavigationMenuCaller";
 import { DropDownNavMenu } from "./ui/NavigationMenuCaller";
 
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
 export default function Header() {
-  const [isDesktop, setDesktop] = useState(false);
-  const [isTablet, setTablet] = useState(false);
-  const [isMobile, setMobile] = useState(false);
-
-  useEffect(() => {
-    const updateMedia = () => {
-      setDesktop(window.innerWidth > 1024);
-      setTablet(window.innerWidth > 768 && window.innerWidth < 1024);
-      setMobile(window.innerWidth > 360 && window.innerWidth < 768);
-    };
-    updateMedia();
-    window.addEventListener("resize", updateMedia);
-    return () => window.removeEventListener("resize", updateMedia);
-  });
 
   return (
     <header className="h-[80px] bg-[var(--background)] filter brightness-115 px-6 md:px-12 lg:px-20 shadow-md sticky top-0 z-50">
@@ -34,10 +19,12 @@ export default function Header() {
             className="inline-block transition duration-300 hover:drop-shadow-[0_0_6px_var(--ash-gray)] min-w-[100px]"
           ></Image>
         </Link>
-        {(isTablet || isDesktop) && (
-          <NavigationMenuCaller></NavigationMenuCaller>
-        )}
-        {isMobile && <DropDownNavMenu></DropDownNavMenu>}
+        <div className="hidden md:block">
+          <NavigationMenuCaller />
+        </div>
+        <div className="block md:hidden">
+          <DropDownNavMenu />
+        </div>
       </div>
     </header>
   );
