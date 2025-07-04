c53e90d0bae6658ba29b77a0f2384c38282de7e1
Miyukiin <jr.concha99@gmail.com>
2025-07-04 15:03:49 +0800

    Style: Ensure responsive text for footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="h-[80px] bg-[var(--background)] filter brightness-115 p-4 shadow-[0px_-5px_10px_rgba(0, 0, 0, 0.2)]">
       <div className="navbar flex flex-[1_1_auto] justify-center h-full mx-10 items-center">
-        <p className="text-center sm:text-sm md:text-base lg:text-base ">© 2025 Jhack Concha. All rights reserved.</p>
+        <p className="text-center text-xs sm:text-sm md:text-base lg:text-base ">© 2025 Jhack Concha. All rights reserved.</p>
       </div>
     </footer>
   );
