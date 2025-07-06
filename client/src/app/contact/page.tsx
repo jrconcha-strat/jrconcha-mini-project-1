d6c150e0480dc99976b6a0c4c33405dad49681bf
Jhack <i_jhack.concha@stratpoint.com>
2025-07-06 13:21:21 +0800

    Feature: Add Contact Page Client-side Form Validation
    Add required attribute, and indicate * for required fields.
    Add maxlength attrib to textarea.
    Adjust minor styles, and responsiveness for socials and contact form,
    such as text sizes at different screen sizes and spacing.

--- client/src/app/contact/page.tsx
+++ client/src/app/contact/page.tsx
@@ -36,11 +36,19 @@ export default function Contact() {
           message using the contact form below.{" "}
         </p>
 
-        <div className="flex flex-col md:flex-row gap-5">
+        <div className="flex flex-col md:flex-row gap-5 lg:items-center mt-5">
           {/* Form */}
-          <div className="border-2 rounded-2xl w-full mt-5 max-w-[700px] min-w-[200px]">
+          <div className="border-2 rounded-2xl w-full max-w-[700px] min-w-[200px]">
             <form action="" method="post" className="flex flex-col p-4 gap-3">
-              <label htmlFor="name">Name:</label>
+              <div className="flex gap-2">
+                <span className="text-base text-[var(--destructive)]">*</span>{" "}
+                <p className="text-sm opacity-70">Indicates a required field</p>
+              </div>
+
+              <label htmlFor="name">
+                Name{" "}
+                <span className="text-base text-[var(--destructive)]">*</span>
+              </label>
               <input
                 type="text"
                 name="name"
@@ -48,8 +56,12 @@ export default function Contact() {
                 autoComplete="off"
                 placeholder="Your Name"
                 className="bg-[var(--input)] text-foreground rounded-sm px-2 p-1"
+                required={true}
               />
-              <label htmlFor="address">Email Address:</label>
+              <label htmlFor="address">
+                Email Address{" "}
+                <span className="text-base text-[var(--destructive)]">*</span>
+              </label>
               <input
                 type="email"
                 name="address"
@@ -57,18 +69,24 @@ export default function Contact() {
                 autoComplete="off"
                 placeholder="Your Email"
                 className="bg-[var(--input)] text-foreground rounded-sm px-2 p-1"
+                required={true}
               />
-              <label htmlFor="message">Message:</label>
+              <label htmlFor="message">
+                Message{" "}
+                <span className="text-base text-[var(--destructive)]">*</span>
+              </label>
               <textarea
                 name="message"
                 id="message"
                 placeholder="Type your message here."
                 autoComplete="on"
                 className="bg-[var(--input)] text-foreground rounded-sm px-2 p-1 h-40 max-h-[300px]"
+                required={true}
+                maxLength={400}
               ></textarea>
               <div className="flex w-full justify-center">
                 <Button
-                  type="button"
+                  type="submit"
                   className="w-full lg:max-w-[200px] hover:drop-shadow-[0_0_6px_var(--foreground)] active:drop-shadow-[0_0_6px_var(--foreground)] hover:scale-105 mt-5"
                 >
                   {" "}
@@ -80,76 +98,84 @@ export default function Contact() {
 
           <div>
             {/* Socials */}
-            <div className="flex flex-wrap gap-x-8 border-2 rounded-2xl w-full mt-5 max-w-[700px] min-w-[200px] p-4 self-start">
+            <div className="flex flex-wrap gap-x-8 border-2 rounded-2xl w-full max-w-[700px] min-w-[200px] p-4 self-start">
               <p className="w-full mb-2 font-semibold"> My Socials:</p>
-              <div>
-                <Link
-                  className="flex gap-3 hover:drop-shadow-[0_0_4px_var(--foreground)] transition-all duration-150 items-center"
-                  href={"mailto:jr.concha99@gmail.com"}
-                >
-                  <FontAwesomeIcon size="xl" icon={faEnvelope} className="" />
-                  <p className="text-sm"> jr.concha99@gmail.com</p>
-                </Link>
-                <Link
-                  className="flex gap-3 hover:drop-shadow-[0_0_4px_var(--foreground)] transition-all duration-150 items-center"
-                  href={"https://www.linkedin.com/in/jhack-concha-3b2a65267/"}
-                >
-                  <FontAwesomeIcon size="xl" icon={faLinkedin} className="" />
-                  <p className="text-sm"> jhack.concha</p>
-                </Link>
-                <Link
-                  className="flex gap-3 hover:drop-shadow-[0_0_4px_var(--foreground)] transition-all duration-150 items-center"
-                  href={"https://github.com/Miyukiin"}
-                >
-                  <FontAwesomeIcon
-                    size="xl"
-                    icon={faSquareGithub}
-                    className=""
-                  />
-                  <p className="text-sm"> Miyukiin</p>
-                </Link>
-              </div>
-              <div>
-                <Link
-                  className="flex gap-3 hover:drop-shadow-[0_0_4px_var(--foreground)] transition-all duration-150 items-center"
-                  href={"https://www.linkedin.com/in/jhack-concha-3b2a65267/"}
-                >
-                  <FontAwesomeIcon
-                    size="xl"
-                    icon={faSquareInstagram}
-                    className=""
-                  />
-                  <p className="text-sm"> jhack.concha</p>
-                </Link>
+              <div className="flex flex-col gap-y-1">
+                <div className="flex flex-col gap-y-1">
+                  <Link
+                    className="flex gap-3 hover:drop-shadow-[0_0_4px_var(--foreground)] transition-all duration-150 items-center"
+                    href={"mailto:jr.concha99@gmail.com"}
+                  >
+                    <FontAwesomeIcon size="xl" icon={faEnvelope} className="" />
+                    <p className="text-sm lg:text-base">
+                      {" "}
+                      jr.concha99@gmail.com
+                    </p>
+                  </Link>
+                  <Link
+                    className="flex gap-3 hover:drop-shadow-[0_0_4px_var(--foreground)] transition-all duration-150 items-center"
+                    href={"https://www.linkedin.com/in/jhack-concha-3b2a65267/"}
+                  >
+                    <FontAwesomeIcon size="xl" icon={faLinkedin} className="" />
+                    <p className="text-sm lg:text-base"> jhack.concha</p>
+                  </Link>
+                  <Link
+                    className="flex gap-3 hover:drop-shadow-[0_0_4px_var(--foreground)] transition-all duration-150 items-center"
+                    href={"https://github.com/Miyukiin"}
+                  >
+                    <FontAwesomeIcon
+                      size="xl"
+                      icon={faSquareGithub}
+                      className=""
+                    />
+                    <p className="text-sm lg:text-base"> Miyukiin</p>
+                  </Link>
+                </div>
+                <div className="flex flex-col gap-y-1">
+                  <Link
+                    className="flex gap-3 hover:drop-shadow-[0_0_4px_var(--foreground)] transition-all duration-150 items-center"
+                    href={"https://www.linkedin.com/in/jhack-concha-3b2a65267/"}
+                  >
+                    <FontAwesomeIcon
+                      size="xl"
+                      icon={faSquareInstagram}
+                      className=""
+                    />
+                    <p className="text-sm lg:text-base"> jhack.concha</p>
+                  </Link>
 
-                <Link
-                  className="flex gap-3 hover:drop-shadow-[0_0_4px_var(--foreground)] transition-all duration-150 items-center"
-                  href={"https://www.linkedin.com/in/jhack-concha-3b2a65267/"}
-                >
-                  <FontAwesomeIcon
-                    size="xl"
-                    icon={faSquareFacebook}
-                    className=""
-                  />
-                  <p className="text-sm"> jhack.concha</p>
-                </Link>
+                  <Link
+                    className="flex gap-3 hover:drop-shadow-[0_0_4px_var(--foreground)] transition-all duration-150 items-center"
+                    href={"https://www.linkedin.com/in/jhack-concha-3b2a65267/"}
+                  >
+                    <FontAwesomeIcon
+                      size="xl"
+                      icon={faSquareFacebook}
+                      className=""
+                    />
+                    <p className="text-sm lg:text-base"> jhack.concha</p>
+                  </Link>
+                </div>
               </div>
             </div>
 
             {/* Contact Information */}
-            <div className="flex flex-wrap gap-x-8 border-2 rounded-2xl w-full mt-5 max-w-[700px] min-w-[200px] p-4 self-start">
-              <p className="w-full mb-2 font-semibold"> Contact Information:</p>
+            <div className="flex flex-wrap gap-x-8 border-2 rounded-2xl w-full mt-3 max-w-[700px] min-w-[200px] p-4 self-start">
+              <p className="w-full mb-2  font-semibold"> Contact Information:</p>
               <div className="flex gap-2">
                 <FontAwesomeIcon size="xl" icon={faPhone} className="" />
                 <p className="w-full mb-3 font-medium "> Phone Number</p>
               </div>
-              <p className="pl-8 w-full mb-4 text-sm md:text-base"> 0926 652 9612</p>
+              <p className="pl-8 w-full mb-2 text-sm md:text-base">
+                {" "}
+                0926 652 9612
+              </p>
 
               <div className="flex gap-2">
                 <FontAwesomeIcon size="xl" icon={faClock} className="" />
                 <p className="w-full mb-3 font-medium"> Working Hours</p>
               </div>
-              <p className="pl-8 w-full mb-4 text-sm md:text-base">
+              <p className="pl-8 w-full mb-2 text-sm md:text-base">
                 {" "}
                 Monday to Friday | 8:00 AM to 5:00 PM
               </p>
@@ -159,7 +185,7 @@ export default function Contact() {
                 <p className="w-full mb-3 font-medium"> Location</p>
               </div>
 
-              <p className="pl-8 w-full mb-4 text-sm md:text-base">
+              <p className="pl-8 w-full mb-2 text-sm md:text-base">
                 {" "}
                 1957 Firmeza St. Sampaloc Manila
               </p>
